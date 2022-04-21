eba82ce6b68a509326e63c0693902a7167f8d434
Matt Joiner <anacrolix@gmail.com>
2022-04-21 14:33:34 +1000

    cmd/dhtup: Add print-tracker-ips-location
    https://gist.github.com/anacrolix/02f7d132866e78101a05bf267d3c7978

--- cmd/dhtup/main.go
+++ cmd/dhtup/main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net"
+	"net/http"
 	"net/url"
 	"os"
 )
 
 func main() {
-	printIpv6TcpTrackers()
+	switch os.Args[1] {
+	case "print-ipv6-trackers":
+		printIpv6TcpTrackers()
+	case "print-tracker-ips-location":
+		printTrackerIpsAndCountries()
+	}
 }
 
 // This consumes input from https://github.com/ngosang/trackerslist
@@ -48,4 +55,75 @@ func printIpv6TcpTrackers() {
 		}
 		fmt.Printf("%v: %v\n", url, ips)
 	}
+}
+
+// This consumes input from https://github.com/ngosang/trackerslist
+func printTrackerIpsAndCountries() {
+	scanner := bufio.NewScanner(os.Stdin)
+	//trackers:
+	for scanner.Scan() {
+		text := scanner.Text()
+		if text == "" {
+			continue
+		}
+		//log.Printf("processing %q", text)
+		url, err := url.Parse(text)
+		if err != nil {
+			log.Printf("parsing %q as url: %v", text, err)
+			continue
+		}
+		host := url.Hostname()
+		ips, err := net.LookupIP(host)
+		if err != nil {
+			log.Printf("looking up ip for %q: %v", host, err)
+			continue
+		}
+		for _, ip := range ips {
+			loc, err := getIpLocation(ip)
+			if err != nil {
+				log.Printf("error getting location for ip %q: %v", ip, err)
+				continue
+			}
+			fmt.Printf("%v\t%v\t%v\t%v\n", loc.Country, loc.City, ip, url)
+		}
+	}
+}
+
+type location struct {
+	Country string
+	City    string
+}
+
+func getIpLocation(ip net.IP) (loc location, err error) {
+	resp, err := http.Get("http://go-geoserve.herokuapp.com/lookup/" + ip.String())
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		err = fmt.Errorf("response status code: %v", resp.StatusCode)
+		return
+	}
+	var dict interface{}
+	err = json.NewDecoder(resp.Body).Decode(&dict)
+	if err != nil {
+		err = fmt.Errorf("decoding response json: %w", err)
+		return
+	}
+	loc.Country = getEnName(dict, "Country")
+	loc.City = getEnName(dict, "City")
+	return
+}
+
+type jsonMap = map[string]interface{}
+
+func getEnName(rootMap interface{}, rootKey string) string {
+	defer func() {
+		r := recover()
+		if r == nil {
+			return
+		}
+		log.Printf("error getting city from %v: %v", rootMap, r)
+	}()
+	return rootMap.(jsonMap)[rootKey].(jsonMap)["Names"].(jsonMap)["en"].(string)
 }
